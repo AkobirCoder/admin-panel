3452b17c8e1478e2e998ff806a3fb01c4d565b4b
AkobirCoder <akobir0308@gmail.com>
2025-09-18 17:40:50 +0500

    ./backend/models/User.js was updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   login: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true }, // 📌 email qo‘shildi
   password: { type: String, required: true }
 });
 
