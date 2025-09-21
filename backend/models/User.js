60537855eb5166ea836ff595f07d5eaefbebe26e
AkobirCoder <akobir0308@gmail.com>
2025-09-21 23:52:58 +0500

    ./backend/models/User.js was updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  login: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+// const UserSchema = new mongoose.Schema({
+//   login: { type: String, required: true, unique: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true }
+// });
 
-module.exports = mongoose.model('User', UserSchema);
+// module.exports = mongoose.model('User', UserSchema);
+
+
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    login: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true } // 👉 createdAt va updatedAt avtomatik qo‘shiladi
+);
+
+module.exports = mongoose.model("User", UserSchema);
