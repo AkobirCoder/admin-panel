83b945e0db72d9e45f4c98359537114788b1c8fb
AkobirCoder <akobir0308@gmail.com>
2025-09-22 23:52:08 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-// import React, { useState } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Landing from "./pages/Landing";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Dashboard from "./pages/Dashboard";
-
-// export default function App() {
-//   const [user, setUser] = useState(null);
-
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Landing />} />
-//         <Route path="/login" element={<Login setUser={setUser} />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser} />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
