b2903b6e56925e3b41869cf3e6374b51efa7e6cc
AkobirCoder <akobir0308@gmail.com>
2025-09-24 23:55:27 +0500

    ./src/index.js was updated

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
 
+root.render(
   <HelmetProvider>
     <App />
   </HelmetProvider>
