4d1f7b8ade0394bab41fa9996250b5d293fc6ef8
AkobirCoder <akobir0308@gmail.com>
2025-09-25 19:46:16 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard/Dashboard";
 import AdminPanel from "./pages/AdminPanel";
+import Profile from "./pages/dashboard/Profile";
+import ChangePassword from "./pages/dashboard/ChangePassword";
 
 export default function App() {
   const [user, setUser] = useState(null);
@@ -16,6 +18,8 @@ export default function App() {
         <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser} />} />
+        <Route path="/dashboard/profile" element={<Profile />} />
+        <Route path="/dashboard/change-password" element={<ChangePassword />} />
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
     </Router>
