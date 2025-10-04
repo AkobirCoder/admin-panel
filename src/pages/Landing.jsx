349909327eda73c712ca89619b2e3c0caa0a296a
AkobirCoder <akobir0308@gmail.com>
2025-10-04 19:49:37 +0500

    ./src/pages/Landing.jsx was updated

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "@dr.pogodin/react-helmet";
 
 export default function Landing({ loggedIn }) {
 
