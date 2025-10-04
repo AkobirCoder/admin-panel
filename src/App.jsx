c3284554af7d7d66f8090c1bf458b03fdd602aaf
AkobirCoder <akobir0308@gmail.com>
2025-10-04 19:49:20 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,3 @@
-// import React, { useState } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Landing from "./pages/Landing";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Dashboard from "./pages/dashboard/Dashboard";
-// import AdminPanel from "./pages/AdminPanel";
-// import Profile from "./pages/dashboard/Profile";
-// import ChangePassword from "./pages/dashboard/ChangePassword";
-
-// export default function App() {
-//   const [user, setUser] = useState(null);
-
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Landing />} />
-//         <Route path="/login" element={<Login setUser={setUser} />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser} />} />
-//         <Route path="/dashboard/profile" element={<Profile />} />
-//         <Route path="/dashboard/change-password" element={<ChangePassword />} />
-//         <Route path="/admin" element={<AdminPanel />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Landing from "./pages/Landing";
@@ -84,3 +55,4 @@ export default function App() {
     </Router>
   );
 }
+
