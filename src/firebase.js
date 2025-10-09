5bf2f1de5fc25adf2cb501531b3f63bc59845326
AkobirCoder <akobir0308@gmail.com>
2025-10-09 12:42:33 +0500

    ./src/firebase.js was updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,19 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBDvTclaIR8Fo07sJeqS2PUDdG2ibhodVg",
+  authDomain: "bilimac-fcfc3.firebaseapp.com",
+  projectId: "bilimac-fcfc3",
+  storageBucket: "bilimac-fcfc3.appspot.com",
+  messagingSenderId: "988247274787",
+  appId: "1:988247274787:web:b9f1668b1a007834d44896",
+  measurementId: "G-8TZX7BGW7W"
 };
 
-// Firebase init
 const app = initializeApp(firebaseConfig);
-
-// Storage va Firestore init
 const storage = getStorage(app);
 const db = getFirestore(app);
 
-export { app, storage, db };
+export { app, storage, db, ref, uploadBytes, getDownloadURL };
