376492e9829b76dcde1cdf2a3af144178e3e0cf4
AkobirCoder <akobir0308@gmail.com>
2025-10-21 09:45:22 +0500

    ./src/i18n.js was updated

---
+++ src/i18n.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import translationUz from "./locales/uz/translation.json";
+import translationRu from "./locales/ru/translation.json";
+import translationEn from "./locales/en/translation.json";
+
+// Har bir til uchun tarjimalar
+const resources = {
+  uz: { translation: translationUz },
+  ru: { translation: translationRu },
+  en: { translation: translationEn },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "uz", // Boshlang‘ich til
+  fallbackLng: "en", // Agar so‘z topilmasa, inglizcha chiqadi
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
